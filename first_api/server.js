ad5a949b7870d687aaae150ffc36bea93b56e36a
haiphung1501 <haiphung15012002@gmail.com>
2022-06-22 23:45:43 +0700

    Building My First API

---
+++ first_api/server.js
@@ -0,0 +1,11 @@
+const { response } = require('express');
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+    response.send('My First API!');
+});
+
+app.listen(3000, function() {
+    console.log('First API running on port 3000!');
+});
